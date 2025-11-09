8f18c383666e82a38e6a9231d09d7ba40b317740
moksha1111 <hazem.salah1@msa.edu.eg>
2025-11-09 11:57:17 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
+import Roadmap from "./components/Roadmap.jsx";
 import Services from "./components/Services";
 
 const App = () => {
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,4 +1,4 @@
-import TagLine from "./Tagline";
+import TagLine from "./Tagline.jsx";
 
 const Heading = ({ className, title, text, tag }) => {
   return (
